28841ec48a8b34f0db46db22e3af4f81c878c1d5
Nick James <verbitan@gmail.com>
2013-01-13 23:03:32 +0000

    Initial commit of template cocos2d + box2d project.

---
+++ hashtag-warrior/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  hashtag-warrior
+//
+//  Created by Nick James on 13/01/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ hashtag-warrior/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  hashtag-warrior
+//
+//  Created by Nick James on 13/01/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
