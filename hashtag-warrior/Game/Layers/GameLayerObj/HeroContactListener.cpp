66cd4d1575477173cf5fc3506e99b4294b8307fe
Nick James <verbitan@gmail.com>
2013-02-18 22:40:56 +0000

    Deleted files accidentally added in commit 6af3e84.

--- hashtag-warrior/Game/Layers/GameLayerObj/HeroContactListener.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  HeroContactListener.cpp
-//  hashtag-warrior
-//
-//  Created by Nick James on 18/02/2013.
-//  Copyright (c) 2013 Ossum Games. All rights reserved.
-//
-
-#include "HeroContactListener.h"
